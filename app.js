a436f31e75a5530c3584d5fc856cc2b85fb2651e
driartbytyqi <driartbytyqii@gmail.com>
2025-03-06 06:19:08 -0800

    driart

--- app.js
+++ app.js
@@ -11,3 +11,21 @@ document.getElementById('searchBar').addEventListener('keyup', function () {
         }
     });
 });
+function validation(){
+    var name = document.getElementById('name').value;
+    var age= document.getElementById('age').value;
+    var valid_name= /^[A-Za-z]+$/;
+    var valid_age= /^[0-9]+$/;
+
+    if(name.match(valid_name) && age.match(valid_age)){
+        return true;
+}else if(!(name.match(valid_name))){
+    document.getElementById('name_error').style.visibility='visible';
+    document.getElementById('name').style.borderColor = 'red';
+    return false;
+}else if(!(age.match(valid_age))){
+    document.getElementById('age_error').style.visibility='visible';
+    document.getElementById('age').style.borderColor = 'red';
+    return false;
+}
+}
