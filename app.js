2b77932994e94d8e789398c3c3dbbac87967176d
lisa5251 <suliqlisa@gmail.com>
2025-03-09 15:57:08 +0100

    peojekt final-lisa

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
-document.getElementById('searchBar').addEventListener('keyup', function () {
-    let searchText = this.value.toLowerCase();
-    let cars = document.querySelectorAll('.car-card');
-
-    cars.forEach(function (car) {
-        let title = car.querySelector('.car-title').textContent.toLowerCase();
-        if (title.includes(searchText)) {
-            car.style.display = "block"; // Show matching cars
-        } else {
-            car.style.display = "none"; // Hide non-matching cars
-        }
+document.getElementById('searchBar').addEventListener('input', function() {
+    let query = this.value.toLowerCase();
+    localStorage.setItem('searchQuery', query); // Store search term
+    let items = document.querySelectorAll('#carInventory .col-md-4');
+    
+    items.forEach(item => {
+        let name = item.getAttribute('data-name').toLowerCase();
+        item.style.display = name.includes(query) ? 'block' : 'none';
     });
 });
-function validation(){
-    var name = document.getElementById('name').value;
-    var age= document.getElementById('age').value;
-    var valid_name= /^[A-Za-z]+$/;
-    var valid_age= /^[0-9]+$/;
-
-    if(name.match(valid_name) && age.match(valid_age)){
-        return true;
-}else if(!(name.match(valid_name))){
-    document.getElementById('name_error').style.visibility='visible';
-    document.getElementById('name').style.borderColor = 'red';
-    return false;
-}else if(!(age.match(valid_age))){
-    document.getElementById('age_error').style.visibility='visible';
-    document.getElementById('age').style.borderColor = 'red';
-    return false;
-}
-}
